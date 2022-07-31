ace8b2880831a101adb416e1706762abbc4a7f06
joeikedo <jikedo@ucsd.edu>
2022-07-30 18:29:16 -0700

    part 2 finished

--- scripts/ButtonCount.js
+++ scripts/ButtonCount.js
@@ -1,1 +1,27 @@
-console.log('hi');
+class ButtonCount extends HTMLElement{
+
+    constructor(){
+        //Inherit extended attributes
+        super();
+
+        const button = document.createElement('button');
+        button.innerText = 'Times Clicked: ';
+
+        const number = document.createElement('span');
+        number.innerText = '0';
+        button.appendChild(number);
+
+        button.addEventListener('click', () =>
+            {
+                let numClicks = parseInt(number.innerText);
+                number.innerText = numClicks + 1;
+            }
+        )
+
+        const shadowDOM = this.attachShadow({mode: 'open'});
+        shadowDOM.appendChild(button);
+    }
+}
+
+//Register in custom elements registry
+customElements.define('button-count', ButtonCount);
